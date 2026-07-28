2ae14745987049e672fc283c059c9783d6658a9d
ki-bun <159973401+ki-bun@users.noreply.github.com>
2026-07-28 12:23:16 +0800

    refactor(settingscreen): optimize and clean code

--- app/src/main/java/com/ki_bun/pioneer/screens/SettingsScreen.kt
+++ app/src/main/java/com/ki_bun/pioneer/screens/SettingsScreen.kt
@@ -1,5 +1,7 @@
 package com.ki_bun.pioneer.screens
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -52,130 +55,73 @@ fun SettingsScreen(
                 modifier = Modifier.fillMaxWidth()
             ) {
 
-                Surface(
-                    color = MaterialTheme.colorScheme.surfaceContainer,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .width(360.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceAround,
+                @Composable
+                fun SettingsButton(screen: String, @DrawableRes iconId: Int, title: String, description: String) {
+                    Surface(
+                        color = MaterialTheme.colorScheme.surfaceContainer,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable(
-                                onClick = {
-                                    navController.navigate("preferencescreen")
-                                }
-                            )
-                            .padding(vertical = 20.dp)
+                            .padding(5.dp)
+                            .width(360.dp)
+                            .clip(RoundedCornerShape(10.dp))
                     ) {
-                        Icon(
-                            painterResource(id = R.drawable.preference_24px),
-                            contentDescription = "Preference icon",
-                            modifier = Modifier.padding(start = 20.dp)
-                        )
-                        Column(
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceAround,
                             modifier = Modifier
-                                .weight(1f)
-                                .padding(start = 20.dp)
+                                .fillMaxWidth()
+                                .clickable(
+                                    onClick = {
+                                        navController.navigate(screen)
+                                    }
+                                )
+                                .padding(vertical = 20.dp)
                         ) {
-                            Text(
-                                text = "App preferences",
-                                fontWeight = FontWeight.Bold
+                            Icon(
+                                painterResource(id = iconId),
+                                contentDescription = title,
+                                modifier = Modifier
+                                    .padding(horizontal = 10.dp)
+                                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+                                    .padding(12.dp),
+                                tint = MaterialTheme.colorScheme.onPrimary
                             )
-                            Text(
-                                text = "Modify the appearance and behavior",
-                                color = MaterialTheme.colorScheme.onSurfaceVariant
-                            )
+                            Column(
+                                modifier = Modifier
+                                    .weight(1f)
+                            ) {
+                                Text(
+                                    text = title,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = description,
+                                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                                )
+                            }
                         }
                     }
                 }
 
-                // Backup
-                Surface(
-                    color = MaterialTheme.colorScheme.surfaceContainer,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .width(360.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceAround,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable(
-                                onClick = {
-                                    navController.navigate("backupscreen")
-                                }
-                            )
-                            .padding(vertical = 20.dp)
-                    ) {
-                        Icon(
-                            painterResource(id = R.drawable.backup_24px),
-                            contentDescription = "Backup icon",
-                            modifier = Modifier.padding(start = 20.dp)
-                        )
-                        Column(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(start = 20.dp)
-                        ) {
-                            Text(
-                                text = "Backup",
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = "Import and export your data",
-                                color = MaterialTheme.colorScheme.onSurfaceVariant
-                            )
-                        }
-                    }
-                }
+                SettingsButton(
+                    "preferencescreen",
+                    R.drawable.preference_24px,
+                    "App preferences",
+                    "Modify the appearance and behavior"
+                )
 
-                // About
-                Surface(
-                    color = MaterialTheme.colorScheme.surfaceContainer,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .width(360.dp)
-                        .clip(RoundedCornerShape(10.dp))
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.SpaceAround,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable(
-                                onClick = {
-                                    navController.navigate("aboutscreen")
-                                }
-                            )
-                            .padding(vertical = 20.dp)
-                    ) {
-                        Icon(
-                            painterResource(id = R.drawable.info_24px),
-                            contentDescription = "Info icon",
-                            modifier = Modifier.padding(start = 20.dp)
-                        )
-                        Column(
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(start = 20.dp)
-                        ) {
-                            Text(
-                                text = "About",
-                                fontWeight = FontWeight.Bold
-                            )
-                            Text(
-                                text = "Details about the app",
-                                color = MaterialTheme.colorScheme.onSurfaceVariant
-                            )
-                        }
-                    }
-                }
+                SettingsButton(
+                    "backupscreen",
+                    R.drawable.backup_24px,
+                    "Backup",
+                    "Import and export your data"
+                )
+
+                SettingsButton(
+                    "aboutscreen",
+                    R.drawable.info_24px,
+                    "About",
+                    "Details about the app"
+                )
 
             }
         }
