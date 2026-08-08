b9d638da2bfe07cd4d539bb5245c9c390b008bdd
ki-bun <159973401+ki-bun@users.noreply.github.com>
2026-08-08 21:26:22 +0800

    build(release): bump version to 2.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.ki_bun.pioneer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.5.0"
+        versionCode = 13
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
