72a8dfceb2727a554d96719aa046fe2daacf76b2
ki-bun <159973401+ki-bun@users.noreply.github.com>
2026-09-04 10:49:57 +0800

    Release v2.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.ki_bun.pioneer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "2.7.0"
+        versionCode = 15
+        versionName = "2.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
